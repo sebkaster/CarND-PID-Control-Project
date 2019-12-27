c26b25a9fdda7687984debbe555ae1ae08283310
sebkaster <sebastiankaster@googlemail.com>
2019-12-27 02:36:12 +0100

    initial commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,22 +9,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+    /**
+     * TODO: Initialize PID coefficients (and errors, if needed)
+     */
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+    /**
+     * TODO: Update PID errors based on cte.
+     */
 
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+    /**
+     * TODO: Calculate and return the total error
+     */
+    return 0.0;  // TODO: Add your total error calc here!
 }
